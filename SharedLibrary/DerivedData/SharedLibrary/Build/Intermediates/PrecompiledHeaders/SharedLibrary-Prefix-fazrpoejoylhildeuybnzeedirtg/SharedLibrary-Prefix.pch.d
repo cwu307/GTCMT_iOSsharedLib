4eb9a799504d9df4df5aef565c30b536508de97e
Govinda Ram Pingali <govind678@gmail.com>
2014-03-08 03:40:55 -0500

    Cleaning Up

--- SharedLibrary/DerivedData/SharedLibrary/Build/Intermediates/PrecompiledHeaders/SharedLibrary-Prefix-fazrpoejoylhildeuybnzeedirtg/SharedLibrary-Prefix.pch.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/govindarampingali/Documents/GaTech/Spring\ 2014/MUSI\ 8903/GTCMT_iOSsharedLib/SharedLibrary/Supporting\ Files/SharedLibrary-Prefix.pch
--- SharedLibrary/DerivedData/SharedLibrary/Build/Intermediates/SharedLibrary.build/Debug-iphonesimulator/SharedLibrary.build/Objects-normal/i386/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/govindarampingali/Documents/GaTech/Spring\ 2014/MUSI\ 8903/GTCMT_iOSsharedLib/SharedLibrary/Supporting\ Files/main.m \
-  /Users/govindarampingali/Documents/GaTech/Spring\ 2014/MUSI\ 8903/GTCMT_iOSsharedLib/SharedLibrary/Source/Application/SharedLibraryAppDelegate.h
