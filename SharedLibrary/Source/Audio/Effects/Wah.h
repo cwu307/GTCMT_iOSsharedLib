64d9d98cffbc44baf8aa80c8ea72d7a97f436bba
cianobri <cianobri@gmail.com>
2014-03-10 13:18:23 -0400

    Process function fix
    Calculate gain within the first loop

--- SharedLibrary/Source/Audio/Effects/Wah.h
+++ SharedLibrary/Source/Audio/Effects/Wah.h
@@ -2,7 +2,7 @@
 #define __Wah_hdr__
 
 #include <Math.h>
-
+#define M_PI 3.14159
 /*	Wah
 	----------------
 	Paramaters:	
@@ -19,8 +19,7 @@ public:
 
 	// set:
 	void setParam(/*hFile::enumType type*/ int type, float value);
-	// get:
-	float getParam(/*hFile::enumType type*/ int type);
+
 	void prepareToPlay(float sampleRate);
 
 	void initDefaults();
@@ -31,6 +30,8 @@ public:
 private:
 
 	float **buff;
+
+	float m_fTempVal;
 
 	float m_fSampleRate;
 	int   m_iNumChannels;
--- SharedLibrary/Source/Audio/Effects/Wah.mm
+++ SharedLibrary/Source/Audio/Effects/Wah.mm
@@ -23,7 +23,7 @@ void CWah::prepareToPlay(float sampleRate)
 void CWah::initDefaults()
 {
 	setParam(0, 1.0);
-	setParam(1, 0.1);
+	setParam(1, 0.3);
 }
 
 void CWah::setParam(/*hFile::enumType type*/ int type, float value)
@@ -32,19 +32,22 @@ void CWah::setParam(/*hFile::enumType type*/ int type, float value)
 	{
 		case 0:
 
-			m_fGain = 0.1*(powf(value, m_fTheta));
+			if (0 <= value && value <= 1)
+				m_fGain = 0.1*(powf(value, m_fTheta));
 
 		break;
 
 		case 1:
 
-			m_fTheta = value;	// "pedal" value
-			m_fQ	 = powf(2.0, 2.0*(1.0 - m_fTheta)+1);
-			m_fReso  = 450.0 * (powf(2.0, 2.3 * m_fTheta));
-			m_fFrn   = m_fReso / m_fSampleRate;
-			m_fPoleAngle =  2.0 * M_PI * m_fFrn;
-			m_fCoeff2	 = -2.0 * (1 - M_PI * m_fFrn / m_fQ) * cos(m_fPoleAngle);
-			m_fCoeff3	 = (1.0 - M_PI * m_fFrn / m_fQ)*(1.0 - M_PI * m_fFrn / m_fQ);
+			if (0 <= value && value <= 1){
+				m_fTheta = value;	// "pedal" value
+				m_fQ	 = powf(2.0, 2.0*(1.0 - m_fTheta)+1);
+				m_fReso  = 450.0 * (powf(2.0, 2.3 * m_fTheta));
+				m_fFrn   = m_fReso / m_fSampleRate;
+				m_fPoleAngle =  2.0 * M_PI * m_fFrn;
+				m_fCoeff2	 = -2.0 * (1 - M_PI * m_fFrn / m_fQ) * cos(m_fPoleAngle);
+				m_fCoeff3	 = (1.0 - M_PI * m_fFrn / m_fQ)*(1.0 - M_PI * m_fFrn / m_fQ);
+			}
 
 		break;
 
@@ -59,18 +62,12 @@ void CWah::process(float **inputBuffer, int numFrames, bool bypass)
 	{
 		for (int c = 0; c < m_iNumChannels; c++)
 		{	
-			inputBuffer[c][i] = inputBuffer[c][i] - m_fCoeff2 * buff[c][0] - m_fCoeff3 * buff[c][1];
 			
-			buff[c][1] = buff[c][0];
-			buff[c][0] = inputBuffer[c][i];
-		}
-	}
-	for (int i = 0; i < numFrames; i++)
-	{
-		for (int c = 0; c < m_iNumChannels; c++)
-		{	
-			inputBuffer[c][i] = m_fGain*inputBuffer[c][i];
+			m_fTempVal		  = inputBuffer[c][i] - m_fCoeff2 * buff[c][0] - m_fCoeff3 * buff[c][1];
+			inputBuffer[c][i] = m_fGain*m_fTempVal;
 
+			buff[c][1] = buff[c][0];
+			buff[c][0] = m_fTempVal;
 		}
 	}
 }
